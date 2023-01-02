338240cd835b7331e52e96bf9529612eeb42a4b1
PrakarshKamal <kamalp@mcmaster.ca>
2023-01-02 17:17:20 -0500

    added Header

---
+++ app/Header.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Bars3Icon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+
+function Header() {
+  return (
+    <header>
+      <div className="grid grid-cols-3 p-10 items-center">
+        <Bars3Icon className="h-8 w-8 cursor-pointer" />
+        <Link href="/" prefetch={false}>
+          <h1 className="font-serif text-4xl text-center">
+            {" "}
+            The{" "}
+            <span className="underline decoration-orange-400 decoration-4">
+              DAILY
+            </span>{" "}
+            Edition{" "}
+          </h1>
+        </Link>
+
+        <div className="flex items-center justify-end space-x-2">
+          {/* Darkmode */}
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
