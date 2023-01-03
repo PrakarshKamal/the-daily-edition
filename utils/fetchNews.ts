ddb4eeff2f74ce99b627788c82314487b3618f8a
PrakarshKamal <kamalp@mcmaster.ca>
2023-01-03 17:28:46 -0500

    created fetch function

---
+++ utils/fetchNews.ts
@@ -0,0 +1,12 @@
+const fetchNews = async (
+  category?: Category | string,
+  keyword?: string,
+  isDyanmic?: boolean
+) => {
+  // GraphQL query
+  // fetch function with Next.js 13 caching
+  // sort function by image vs not image present
+  // return sorted array
+};
+
+export default fetchNews;
