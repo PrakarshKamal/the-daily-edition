086cf5b93e45e99b6c5534c8d73cbfff7e8ab7e9
PrakarshKamal <kamalp@mcmaster.ca>
2023-01-04 16:59:11 -0500

    live data from api or mock data from response

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { categories } from "../constants";
 import fetchNews from "../utils/fetchNews";
+import NewsList from "./NewsList";
+import response from "../response.json";
 
 async function Home() {
   // fetch news data
-  const news: NewsResponse = await fetchNews(categories.join(","));
+  const news: NewsResponse =
+    response || (await fetchNews(categories.join(",")));
 
-  return <div>{/* NewsList  */}</div>;
+  return (
+    <div>
+      <NewsList news={news} />
+    </div>
+  );
 }
 
 export default Home;
