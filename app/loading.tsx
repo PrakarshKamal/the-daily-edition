d69cec7df3dbbfd5e7d9e64f84916999a1dc07d3
PrakarshKamal <kamalp@mcmaster.ca>
2023-01-04 17:02:14 -0500

    page loader

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <div className="animate-pulse text-xl text-gray-700 p-10 text-center font-serif">
+      Loading news feed...
+    </div>
+  );
+}
+
+export default Loading;
