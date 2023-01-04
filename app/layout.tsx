4cf0554efb7284ccc440bc687fd8ced617242e67
PrakarshKamal <kamalp@mcmaster.ca>
2023-01-04 16:59:43 -0500

    add providers for dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import "../styles/globals.css";
 import Header from "./Header";
 import Providers from "./Providers";
@@ -9,12 +11,14 @@ export default function RootLayout({
 }) {
   return (
     <html>
-      <Providers>
-        <body className="bg-gray-100 dark:bg-zinc-900 transition-all duration-700">
-          <Header />
-          <div className="max-w-6xl mx-auto">{children}</div>
-        </body>
-      </Providers>
+      <head>
+        <Providers>
+          <body className="bg-gray-100 dark:bg-zinc-900 transition-all duration-700">
+            <Header />
+            <div className="max-w-6xl mx-auto">{children}</div>
+          </body>
+        </Providers>
+      </head>
     </html>
   );
 }
